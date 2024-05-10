97bf8956cb5c43e1f8ceffebe3bb688fcaab412e
GabrielDavid1 <biocamendeschico@gmail.com>
2024-05-10 12:34:41 -0300

    Up

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import SvgModel from "@/utils/svg";
 
 export default function Home() {
@@ -5,6 +7,7 @@ export default function Home() {
     <main>
       <section className="initial flex">
         <div className="initialArea flex">
+
           <div className="description-area">
             <h2 className="description-title">
               Receba doações <br />com <span className="trueMoney">dinheiro</span> de verdade
@@ -24,19 +27,107 @@ export default function Home() {
               </button>
             </div>
           </div>
+
         </div>
       </section>
       <section className="topic flex">
-        <h2> topico </h2>
+
+        <div className="card-privacy flex fd">
+          <div className="cardHead">
+            <h2> Privacidade </h2>
+          </div>
+          <div className="cardBody flex">
+            <SvgModel
+              name="privacity"
+              width="75%"
+              height="75%"
+            />
+          </div>
+          <div className="cardFooter flex">
+            <h2>Seus seguidores que contribuem com doações podem permanecer completamente em anonimato. Além das próprias doações ficarem salvas com você e não no sistema</h2>
+          </div>
+        </div>
+
+        <div className="card-ai flex fd">
+          <div className="cardHead">
+            <h2> Inteligência artificial </h2>
+          </div>
+          <div className="cardBody flex">
+            <SvgModel
+              name="brain-ai"
+              width="75%"
+              height="75%"
+            />
+          </div>
+          <div className="cardFooter flex">
+            <h2>
+              Seus seguidores podem utilizar o modelo de inteligência artificial na exibição de doações assim como diversos outros serviços</h2>
+          </div>
+        </div>
+
+        <div className="card-fee flex fd">
+          <div className="cardHead">
+            <h2> Taxa 2.50%  </h2>
+          </div>
+          <div className="cardBody flex">
+            <SvgModel
+              name="fee"
+              width="100%"
+              height="100%"
+            />
+          </div>
+          <div className="cardFooter flex">
+            <h2>Seus seguidores podem utilizar o modelo de inteligência artificial na exibição de doações</h2>
+          </div>
+        </div>
+
       </section>
       <section className="howto flex">
-        <h2> como usar </h2>
+        <div className="wrappedArea flex">
+          <div className="videoSection">
+            <Image
+              id="cellImage"
+              width={740}
+              height={385}
+              src="/img/cell.jpg"
+              alt="cell"
+            />
+            <video
+              id="video"
+              disablePictureInPicture
+              controls={false}
+              playsInline
+              autoPlay
+              loop
+              muted
+            >
+              <source src={''} />
+            </video>
+          </div>
+          <div className="descriptionArea flex fd">
+            <div className="titleArea wh100">
+              <h2>Como usar ?</h2>
+              <h3> 1° - Abra a pagina inicial <br /> </h3>
+              <h3> 2° - Crie seu hub de acordo com suas preferências de configuração <br /> </h3>
+              <h3> 3° - Importe o hub com a chave gerada e gerêncie suas doações <br /> </h3>
+              <h3> 4° - Utilizar </h3>
+              <button id="paperButton">Ler paper</button>
+            </div>
+          </div>
+        </div>
       </section>
       <section className="lightning-area flex">
-        <h2> lightning </h2>
+        <h2> Transações realizadas via Lightning Network </h2>
       </section>
       <section className="faq flex">
-        <h2> como usar </h2>
+        <div className="faq-area flex fd">
+          <div className="icon-area flex">
+            <h2>SatsFI</h2>
+          </div>
+          <div className="tou-area">
+            <h2>Termos de uso</h2>
+          </div>
+        </div>
       </section>
     </main>
   );
---
+++ utils/svg/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Svg = styled.svg`
+    
+    animation: svgFloat 1.4s ease infinite;
+    
+    @keyframes svgFloat {
+        0% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(-10px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+
+`;
